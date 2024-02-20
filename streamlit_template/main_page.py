8163a48b770934b98c33f67191b6f97f34293fb3
Troy <tsacekay2000@yahoo.com>
2024-02-19 22:05:44 -0700

    Update main_page.py 2

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -5,5 +5,5 @@ st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer or kart stats")
 
-link = '[To my Github Pages Site](https://tsacekay2000.github.io/INFO4120/)'
+link = '[To my Github Pages Site](https://tsacekay2000.github.io/INFO4120)'
 st.markdown(link, unsafe_allow_html=True)
